2b3bba2f22722cb342e3648289fc66cee7ed376b
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-08-16 11:43:20 +0530

    [Moulidharan] Added: Implementing the Jinja templates for APIs

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,8 @@ from user import views
 urlpatterns = [
     path("create_user/", views.UserRegistration.as_view(), name="create"),
     path("login_user/", views.UserLogin.as_view(), name="login"),
+    path("user_register/", views.JinjaRegistration.as_view(), name="register"),
+    path("user_login/", views.JinjaLogin.as_view(), name="login"),
+    path("index/", views.home_page, name="index"),
+    path("user_logout/", views.user_logout, name="logout")
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from logs import logger
 from rest_framework.views import APIView
+from .models import User
 from .serializers import RegisterSerializers, LoginSerializers
 from rest_framework.response import Response
 from rest_framework import status
 from .utils import JWToken
 from drf_yasg.utils import swagger_auto_schema
+from django.views.generic import View
+from django.contrib import messages
 
 
 class UserRegistration(APIView):
@@ -34,3 +39,48 @@ class UserLogin(APIView):
         except Exception as ex:
             logger.exception(ex)
             return Response({"message": str(ex), "status": 400, "data": {}}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class JinjaRegistration(View):
+    def get(self, request):
+        return render(request, 'UserRegister.html')
+
+    def post(self, request):
+        try:
+            data = {x: y for x, y in request.POST.items()}
+            data.pop('csrfmiddlewaretoken')
+            User.objects.create_user(**data)
+            messages.info(request, "User Registered Successfully")
+            return redirect('login')
+        except Exception as ex:
+            messages.error(request, str(ex))
+            return render(request, 'UserRegister.html')
+
+
+class JinjaLogin(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('index')
+        return render(request, 'UserLogin.html')
+
+    def post(self, request):
+        try:
+            data = {x: y for x, y in request.POST.items()}
+            data.pop('csrfmiddlewaretoken')
+            user = authenticate(**data)
+            if not user:
+                raise Exception("Invalid Credentials")
+            login(request, user)
+            return redirect('index')
+        except Exception as ex:
+            messages.error(request, str(ex))
+            return render(request, 'UserLogin.html')
+
+
+def home_page(request):
+    return render(request, 'index.html', context={"user": request.user})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
